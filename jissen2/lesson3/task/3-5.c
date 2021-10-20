bc0d16d22e7c642ce401ce806e4340104db0af53
Jtwulf <jtwulf1005@icloud.com>
2021-10-20 15:53:35 +0900

    jissen2 lesson3 task done

--- jissen2/lesson3/task/3-5.c
+++ jissen2/lesson3/task/3-5.c
@@ -8,10 +8,16 @@ int main(){
 	printf("2次元配列の列数を入力 -> ");
 	scanf("%d", &rc);
 
-	int **p = (int **)malloc(sizeof(int) * (nr*rc));
+	int **p;
+	*p = (int *)malloc(sizeof(int) * (nr*rc));
 	for(int i = 0; i < nr; i++){
 		for(int j = 0; j < rc; j++){
-			*(*(p+i)+j)= i+j;
+			printf("%d行目%d列目の値を入力 -> ",i+1,j+1);
+			scanf("%d",&p[i][j]);
+		}
+	}
+	for(int i = 0; i < nr; i++){
+		for(int j = 0; j < rc; j++){
 			printf("%d\t",p[i][j]);
 		}
 		printf("\n");
