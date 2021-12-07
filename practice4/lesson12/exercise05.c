d313753e83288da5496fee892356189a8822971d
Jtwulf <jtwulf1005@icloud.com>
2021-12-07 11:43:44 +0900

    2021/12/07

---
+++ practice4/lesson12/exercise05.c
@@ -0,0 +1,157 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define DELETED 1
+#define BUFSIZE 100
+#define WORDSIZE 31
+#define FIRST 0
+#define FAILED 0
+
+int buf[BUFSIZE];
+int bufp = 0;
+
+typedef struct element {
+  int value;
+  struct element *next;
+} element_type;
+
+void putlist(element_type *q);
+void getlist(element_type **pp);
+int getword(char *word, int lim);
+int getch(void);
+void ungetch(int c);
+int list_sum(element_type *p);
+void eraselist(element_type **pp);
+
+
+
+int main(void) {
+  element_type *root;
+  int status;
+
+  printf("リストに入れる正整数を入力して下さい。\n");
+  printf("入力は 0 以下の整数を入力したときに終わります\n");
+  getlist(&root);
+
+  printf("できあがったリストを出力します。\n");
+  putlist(root);
+  putchar('\n');
+
+  printf("リストの中の最初の偶数を削除し、結果を出力します。\n");
+  status = delete_one_even(&root);
+  if (status == FAILED) {
+    printf("偶数はありませんでした。\n");
+    printf("そのまま出力します\n");
+  }
+  putlist(root);
+  putchar('\n');
+
+  printf("リストを消去します。\n");
+  eraselist(&root);
+
+
+  return 0;
+}
+
+/*   ここに関数 delete_one_even を記述します。   */
+
+
+void getlist(element_type **pp)
+{
+  element_type *cur_p, *pre_p;
+  int num, count, kind;
+  char word[WORDSIZE];
+
+  *pp = NULL;
+  count = FIRST;
+  printf("要素値を入力＞ ");
+  getword(word, BUFSIZE);
+  num = atoi(word);
+  while (num > 0) {
+    cur_p = (element_type *)malloc(sizeof (element_type));
+    if (count == FIRST) {
+      *pp = cur_p;
+    } else {
+      pre_p->next = cur_p;
+    }
+    cur_p->value = num;
+    cur_p->next = NULL;
+    pre_p = cur_p;
+    count++;
+    printf("要素値を入力＞ ");
+    getword(word, BUFSIZE);
+    num = atoi(word);
+  }
+}
+
+void putlist(element_type *q)
+{
+  while (q != NULL) {
+    printf("%2d ", q->value);
+    q = q->next;
+  }
+}
+
+int getword(char *word, int lim)
+{
+  int c;
+  char *w = word;
+
+  while (isspace(c = getch()))
+    ;
+  if (c != EOF) {
+    *w++ = c;
+  }
+  if (!isalnum(c) && c != '+' && c != '-') {
+      *w = '\0';
+      return c;
+  } else if (isalpha(c)) {
+      for ( ; --lim > 0; w++) {
+        if (!isalnum(*w = getch())) {
+          ungetch(*w);
+          break;
+        }
+      }
+      *w = '\0';
+      return word[0];
+  } else {
+      for ( ; --lim > 0; w++) {
+        if (!isdigit(*w = getch())) {
+          ungetch(*w);
+          break;
+        }
+      }
+      *w = '\0';
+      return word[0];
+  }
+}
+
+int getch(void)
+{
+  if (bufp > 0) {
+    return buf[--bufp];
+  } else {
+    return getchar();
+  }
+}
+
+void ungetch(int c)
+{
+  if (bufp >= BUFSIZE) {
+    printf("ungetch : 押し戻す文字が多すぎます\n");
+  } else {
+    buf[bufp++] = c;
+  }
+}
+
+void eraselist(element_type **pp)
+{
+  element_type *q;
+
+  while (*pp != NULL) {
+    q = *pp;
+    *pp = (*pp)->next;
+    free(q);
+  }
+}
---
+++ practice4/lesson12/exercise08.c
@@ -0,0 +1,156 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define DELETED 1
+#define BUFSIZE 100
+#define WORDSIZE 31
+#define FIRST 0
+#define FAILED 0
+
+int buf[BUFSIZE];
+int bufp = 0;
+
+typedef struct element {
+  int value;
+  struct element *next;
+} element_type;
+
+void putlist(element_type *q);
+void getlist(element_type **pp);
+int getword(char *word, int lim);
+int getch(void);
+void ungetch(int c);
+void eraselist(element_type **pp);
+void insert_before_all_even(element_type **pp, int val);
+
+
+int main(void) {
+  element_type *root;
+  char word[WORDSIZE];
+  int num;
+
+  printf("リストに入れる正整数を入力して下さい。\n");
+  printf("入力は 0 以下の整数を入力したときに終わります\n");
+  getlist(&root);
+
+  printf("できあがったリストを出力します。\n");
+  putlist(root);
+  putchar('\n');
+
+  printf("整数を入力し、その値をリスト中の全ての偶数の前に挿入します。\n");
+  printf("要素値を入力＞ ");
+  getword(word, BUFSIZE);
+  num = atoi(word);
+  insert_before_all_even(&root, num);
+  putlist(root);
+  putchar('\n');
+
+  printf("リストを消去します。\n");
+  eraselist(&root);
+
+
+  return 0;
+}
+
+/*   ここに関数 insert_before_all_even を記述します。   */
+
+
+void getlist(element_type **pp)
+{
+  element_type *cur_p, *pre_p;
+  int num, count, kind;
+  char word[WORDSIZE];
+
+  *pp = NULL;
+  count = FIRST;
+  printf("要素値を入力＞ ");
+  getword(word, BUFSIZE);
+  num = atoi(word);
+  while (num > 0) {
+    cur_p = (element_type *)malloc(sizeof (element_type));
+    if (count == FIRST) {
+      *pp = cur_p;
+    } else {
+      pre_p->next = cur_p;
+    }
+    cur_p->value = num;
+    cur_p->next = NULL;
+    pre_p = cur_p;
+    count++;
+    printf("要素値を入力＞ ");
+    getword(word, BUFSIZE);
+    num = atoi(word);
+  }
+}
+
+void putlist(element_type *q)
+{
+  while (q != NULL) {
+    printf("%2d ", q->value);
+    q = q->next;
+  }
+}
+
+int getword(char *word, int lim)
+{
+  int c;
+  char *w = word;
+
+  while (isspace(c = getch()))
+    ;
+  if (c != EOF) {
+    *w++ = c;
+  }
+  if (!isalnum(c) && c != '+' && c != '-') {
+      *w = '\0';
+      return c;
+  } else if (isalpha(c)) {
+      for ( ; --lim > 0; w++) {
+        if (!isalnum(*w = getch())) {
+          ungetch(*w);
+          break;
+        }
+      }
+      *w = '\0';
+      return word[0];
+  } else {
+      for ( ; --lim > 0; w++) {
+        if (!isdigit(*w = getch())) {
+          ungetch(*w);
+          break;
+        }
+      }
+      *w = '\0';
+      return word[0];
+  }
+}
+
+int getch(void)
+{
+  if (bufp > 0) {
+    return buf[--bufp];
+  } else {
+    return getchar();
+  }
+}
+
+void ungetch(int c)
+{
+  if (bufp >= BUFSIZE) {
+    printf("ungetch : 押し戻す文字が多すぎます\n");
+  } else {
+    buf[bufp++] = c;
+  }
+}
+
+void eraselist(element_type **pp)
+{
+  element_type *q;
+
+  while (*pp != NULL) {
+    q = *pp;
+    *pp = (*pp)->next;
+    free(q);
+  }
+}
