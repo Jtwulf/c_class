f4e3a20999ccf53e6fc38cc00dcccebee25719bb
Jtwulf <76027910+Jtwulf@users.noreply.github.com>
2021-12-21 12:10:01 +0900

    2021/12/21

---
+++ practice4/lesson14/14-1.c
@@ -0,0 +1,206 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct node {
+	int key;
+	char data[100];
+	struct node *left;
+	struct node *right;
+} node_type;
+
+int ins_binst(node_type **root, int key, char *data);
+node_type *search_binst(node_type *root, int target, node_type new_data);
+void free_node(node_type *root);
+void show_tree(node_type *root);
+node_type *search_cp(node_type *root, node_type *a, node_type *b);
+node_type *search_max(node_type *root);
+void delete_node(node_type **root, int key);
+node_type *search_min(node_type **p);
+
+int main(){
+	int key, flag, target;
+	char data[100];
+	node_type *res, *root = NULL;
+
+	printf("key dataを入力 -> ");
+	scanf("%d %s", &key, data);
+	while (key != 0){
+		flag = ins_binst(&root, key, data);
+		printf("key dataを入力 -> ");
+		scanf("%d %s", &key, data);
+	}
+
+	if (root == NULL){
+		printf("ノードを一つも挿入していません\n");
+		return 0;
+	}
+
+	printf("新しく挿入するノードのキー値を入力 -> ");
+	scanf("%d",&key);
+	printf("新しく挿入するノードのデータを入力 -> ");
+	scanf("%s",data);
+	printf("置き換えるノードのキー値を入力 -> ");
+	scanf("%d",&target);
+	node_type new_data;
+	new_data.key = key;
+	strcpy(new_data.data,data);
+	search_binst(root,target,new_data);
+
+	show_tree(root);
+
+	free_node(root);
+	return 0;
+}
+
+node_type *search_binst(node_type * root, int target, node_type new_data){
+	node_type *p = root, *buf = p;
+	while (p != NULL){
+		if (target == p->key){
+			p->key = new_data.key;
+			strcpy(p->data,new_data.data);
+			return p;
+		}
+		else if (target < p->key)
+			p = p->left;
+		else
+			p = p->right;
+	}
+	return NULL;
+}
+
+void delete_node(node_type **root, int key){
+	node_type **p, *x;
+	p = root;
+	while(*p != NULL){
+		if(key == (*p)->key){
+			x = *p;
+			if(x->left == NULL && x->right == NULL)
+				*p = NULL;
+			else if(x->left == NULL)
+				*p = x->right;
+			else if(x->right == NULL)
+				*p = x->left;
+			else {
+				*p = search_min(&(x->right));
+				(*p)->right = x->right;
+				(*p)->left = x->left;
+			}
+			free(x);
+			return;
+		} else if(key < (*p)->key)
+			p = &((*p)->left);
+		else
+			p = &((*p)->right);
+	}
+	return;
+}
+
+node_type *search_min(node_type **p){
+	node_type *min;
+	while((*p)->left != NULL)
+		p = &((*p)->left);
+	min = *p;
+	*p = (*p)->right;
+	return min;
+}
+
+node_type *search_cp(node_type *root, node_type *a, node_type *b){
+	node_type *p = root;
+	while(p != NULL){
+		if(a->key < p->key && b->key < p->key)
+			p = p->left;
+		else if(a->key > p->key && b->key > p->key)
+			p = p->right;
+		else
+			return p;
+	}
+}
+
+/*
+node_type *search_binst(node_type * root, int target){
+	node_type *p = root;
+	while (p != NULL){
+		if (target == p->key)
+			return p;
+		else if (target < p->key)
+			p = p->left;
+		else
+			p = p->right;
+	}
+	return NULL;
+}
+*/
+
+int ins_binst(node_type **root, int key, char *data)
+{
+	node_type *p, *q, *r;
+	r = (node_type *)malloc(sizeof(node_type));
+	r->key = key;
+	strcpy(r->data, data);
+	r->right = NULL;
+	r->left = NULL;
+
+	if (*root == NULL){
+		*root = r;
+		return 1;
+	} else {
+		p = *root;
+		while(p != NULL){
+			if(key == p->key) return 0;
+			else {
+				q = p;
+				if(key < p->key) p = p->left;
+				else p = p->right;
+			}
+		}
+		if(key < q->key) q->left = r;
+		else q->right = r;
+	}
+	return 1;
+}
+
+void free_node(node_type *root){
+	if(root->left != NULL)
+		free_node(root->left);
+	if(root->right != NULL)
+		free_node(root->right);
+	free(root);
+}
+
+/*
+void show_tree(node_type *root){
+	if(root->right != NULL)
+		show_tree(root->right);
+	if(root->left != NULL)
+		show_tree(root->left);
+	printf("%d ", root->key);
+}
+*/
+
+void show_tree(node_type *root){
+	node_type *p = root;
+	printf("%d: %s ",p->key, p->data);
+	if(p->left != NULL)
+		printf("left: %d ",p->left->key);
+	if(p->right != NULL)
+		printf("right: %d ",p->right->key);
+	printf("\n");
+	if(root->right != NULL)
+		show_tree(root->right);
+	if(root->left != NULL)
+		show_tree(root->left);
+}
+
+node_type* search_max(node_type *root){
+	node_type *r = root, *max_node = r;
+	int max = 0;
+	while(r != NULL){
+		if(max < r->key){
+			max = r->key;
+			max_node = r;
+		}
+		r = r->left;
+	}
+	return max_node;
+}
---
+++ practice4/lesson14/14-2.c
@@ -0,0 +1,202 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct node {
+	int key;
+	char data[100];
+	struct node *left;
+	struct node *right;
+} node_type;
+
+int ins_binst(node_type **root, int key, char *data);
+node_type *search_binst(node_type *root, int target);
+void free_node(node_type *root);
+void show_tree(node_type *root);
+node_type *search_cp(node_type *root, node_type *a, node_type *b);
+node_type *search_max(node_type *root);
+void delete_node(node_type **root, int key);
+node_type *search_min(node_type **p);
+void search_max_min(node_type *root, node_type **max_pp, node_type **min_pp);
+
+int main(){
+	int key, flag;
+	char data[100];
+	node_type *res, *root = NULL;
+
+	printf("key dataを入力 -> ");
+	scanf("%d %s", &key, data);
+	while (key != 0){
+		flag = ins_binst(&root, key, data);
+		printf("key dataを入力 -> ");
+		scanf("%d %s", &key, data);
+	}
+
+	if (root == NULL){
+		printf("ノードを一つも挿入していません\n");
+		return 0;
+	}
+
+	node_type *max_node, *min_node;
+	search_max_min(root, &max_node, &min_node);
+	printf("最大のキー値を持つノードは key: %d, data: %s\n",max_node->key, max_node->data);
+	printf("最小のキー値を持つノードは key: %d, data: %s\n",min_node->key, min_node->data);
+
+	show_tree(root);
+
+	free_node(root);
+	return 0;
+}
+
+void delete_node(node_type **root, int key){
+	node_type **p, *x;
+	p = root;
+	while(*p != NULL){
+		if(key == (*p)->key){
+			x = *p;
+			if(x->left == NULL && x->right == NULL)
+				*p = NULL;
+			else if(x->left == NULL)
+				*p = x->right;
+			else if(x->right == NULL)
+				*p = x->left;
+			else {
+				*p = search_min(&(x->right));
+				(*p)->right = x->right;
+				(*p)->left = x->left;
+			}
+			free(x);
+			return;
+		} else if(key < (*p)->key)
+			p = &((*p)->left);
+		else
+			p = &((*p)->right);
+	}
+	return;
+}
+
+node_type *search_min(node_type **p){
+	node_type *min;
+	while((*p)->left != NULL)
+		p = &((*p)->left);
+	min = *p;
+	*p = (*p)->right;
+	return min;
+}
+
+node_type *search_cp(node_type *root, node_type *a, node_type *b){
+	node_type *p = root;
+	while(p != NULL){
+		if(a->key < p->key && b->key < p->key)
+			p = p->left;
+		else if(a->key > p->key && b->key > p->key)
+			p = p->right;
+		else
+			return p;
+	}
+}
+
+node_type *search_binst(node_type * root, int target){
+	node_type *p = root;
+	while (p != NULL){
+		if (target == p->key)
+			return p;
+		else if (target < p->key)
+			p = p->left;
+		else
+			p = p->right;
+	}
+	return NULL;
+}
+
+int ins_binst(node_type **root, int key, char *data)
+{
+	node_type *p, *q, *r;
+	r = (node_type *)malloc(sizeof(node_type));
+	r->key = key;
+	strcpy(r->data, data);
+	r->right = NULL;
+	r->left = NULL;
+
+	if (*root == NULL){
+		*root = r;
+		return 1;
+	} else {
+		p = *root;
+		while(p != NULL){
+			if(key == p->key) return 0;
+			else {
+				q = p;
+				if(key < p->key) p = p->left;
+				else p = p->right;
+			}
+		}
+		if(key < q->key) q->left = r;
+		else q->right = r;
+	}
+	return 1;
+}
+
+void free_node(node_type *root){
+	if(root->left != NULL)
+		free_node(root->left);
+	if(root->right != NULL)
+		free_node(root->right);
+	free(root);
+}
+
+/*
+void show_tree(node_type *root){
+	if(root->right != NULL)
+		show_tree(root->right);
+	if(root->left != NULL)
+		show_tree(root->left);
+	printf("%d ", root->key);
+}
+*/
+
+void show_tree(node_type *root){
+	node_type *p = root;
+	printf("%d: %s ",p->key, p->data);
+	if(p->left != NULL)
+		printf("left: %d ",p->left->key);
+	if(p->right != NULL)
+		printf("right: %d ",p->right->key);
+	printf("\n");
+	if(root->right != NULL)
+		show_tree(root->right);
+	if(root->left != NULL)
+		show_tree(root->left);
+}
+
+node_type* search_max(node_type *root){
+	node_type *r = root, *max_node = r;
+	int max = 0;
+	while(r != NULL){
+		if(max < r->key){
+			max = r->key;
+			max_node = r;
+		}
+		r = r->left;
+	}
+	return max_node;
+}
+
+void search_max_min(node_type *root, node_type **max_pp, node_type **min_pp){
+	node_type *r = root;
+	int max = r->key, min = r->key;
+	if(r != NULL){
+		if(max < r->key){
+			max = r->key;
+			*max_pp = r;
+		}
+		if(min > r->key){
+			min = r->key;
+			*(min_pp) = r;
+		}
+		if(r->left != NULL)
+			search_max_min(r->left, max_pp, min_pp);
+		if(r->right != NULL)
+			search_max_min(r->right, max_pp, min_pp);
+	}
+}
